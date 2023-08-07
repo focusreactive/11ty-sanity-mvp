b07838c2d7e54c90fe94e90da99de36dbe6fe292
Alex Hramovich <alex.gramovich.1996@gmail.com>
2023-08-07 10:19:03 +0200

    add live preview

--- cms/utils/deskStructure.jsx
+++ cms/utils/deskStructure.jsx
@@ -2,10 +2,10 @@ import React from "react"
 
 const JsonPreview = ({ document }) => {
     console.log(document)
-    return <iframe title='page' src={`https://11ty-sanity-mvp.vercel.app/${document.displayed?.slug?.current}`} style={{height: '100%', width: '100%'}} />
+    return <iframe title='page' src={`http://localhost:3000/dynamic?slug=${document.displayed?.slug?.current}`} style={{height: '100%', width: '100%'}} />
 }
 export const defaultDocumentNodeResolver = (S) =>
     S.document().views([
         S.view.form(),
-        S.view.component(JsonPreview).title('JSON'),
+        S.view.component(JsonPreview).title('Preview'),
     ])
