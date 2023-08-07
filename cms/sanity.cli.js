d73a4cd870e49f0359135fa872ba4857d29d349e
Alex Hramovich <alex.gramovich.1996@gmail.com>
2023-08-07 12:26:37 +0200

    add preview

--- cms/sanity.cli.js
+++ cms/sanity.cli.js
@@ -4,5 +4,8 @@ export default defineCliConfig({
   api: {
     projectId: 'nzudkmke',
     dataset: 'production'
-  }
+  },
+  vite: {
+    external: ['part:@sanity/base/datastore/document'],
+  },
 })
--- cms/utils/deskStructure.jsx
+++ cms/utils/deskStructure.jsx
@@ -1,65 +1,44 @@
-import React, {useEffect} from 'react'
+import React, {useCallback, useEffect, useState} from 'react'
+import {createClient} from '@sanity/client'
+import debounce from 'lodash.debounce'
 
-function throttle(func, delay) {
-  let timeoutId
-  let lastExecTime = 0
+const client = createClient({
+  projectId: 'nzudkmke',
+  dataset: 'production',
+  token:
+    'sk4pvS2ZY8GiQuEiYsW4eIEQ9Vgcuh1xhMnL80ch5sA3ozTtHkpJfqJItI4pyWvT2ijrE2Mlszuee9hPTlTq0U3yy54jTcdPohzqms00vxfzIpzOFqdOOtuMSTAiUEFaVa2i3XVjUpLxIPjclRbCqxM0d85dqv7cigcTOiTEC2jsYfYLRt3w',
+  useCdn: false,
+})
 
-  return function (...args) {
-    const currentTime = Date.now()
-    const timeSinceLastExec = currentTime - lastExecTime
+function useForceUpdate() {
+  const [value, setValue] = useState(0)
 
-    if (!timeoutId && timeSinceLastExec >= delay) {
-      func.apply(this, args)
-      lastExecTime = currentTime
-    } else if (!timeoutId) {
-      timeoutId = setTimeout(() => {
-        func.apply(this, args)
-        lastExecTime = Date.now()
-        timeoutId = null
-      }, delay - timeSinceLastExec)
-    }
+  return {
+    trigger: () => {
+      setTimeout(() => {
+        setValue((value) => value + 1)
+      }, 1500)
+    },
+    value,
   }
 }
 
-const JsonPreview = ({document}) => {
+const JsonPreview = ({document: sanityDocument}) => {
+  const {trigger, value} = useForceUpdate()
+  const debouncedChangeHandler = useCallback(debounce(trigger, 1500), [])
+
   useEffect(() => {
-    const rawQuery = `*[_type == "pages" && slug.current == "{{ eleventy.serverless.query.slug }}" && _id in path("drafts.**")][0]`
-    const rawQuery2 = `*[_type == "pages" && !(_id in path("drafts.**"))][0]`
-    const searchParams = new URLSearchParams({query: rawQuery}).toString()
-    const searchParams2 = new URLSearchParams({query: rawQuery2}).toString()
-
-    const myFunction = () => {
-      setTimeout(function () {
-        document.getElementById('preview_iframe').contentWindow.location.reload(true)
-      }, 2000)
+    if (debouncedChangeHandler) {
+      debouncedChangeHandler()
     }
-
-    const listener = throttle(myFunction, 1500)
-    var source = new EventSource(
-      `https://nzudkmke.api.sanity.io/v2021-06-07/data/listen/production?${searchParams}`,
-      {withCredentials: true},
-    )
-    source.addEventListener('mutation', listener)
-
-    const myFunction2 = () => {
-      setTimeout(function () {
-        document.getElementById('preview_iframe').contentWindow.location.reload(true)
-      }, 2000)
-    }
-
-    const listener2 = throttle(myFunction2, 1500)
-    var source2 = new EventSource(
-      `https://nzudkmke.api.sanity.io/v2021-06-07/data/listen/production?${searchParams2}`,
-      {withCredentials: true},
-    )
-    source2.addEventListener('mutation', listener2)
-  }, [])
+  }, [sanityDocument?.displayed?._updatedAt, debouncedChangeHandler])
 
   return (
     <iframe
       title="page"
       id="preview_iframe"
-      src={`https://11ty-sanity-mvp.vercel.app/api/preview?slug=${document.displayed?.slug?.current}`}
+      key={value}
+      src={`https://11ty-sanity-mvp.vercel.app/api/preview?slug=${sanityDocument.displayed?.slug?.current}`}
       style={{height: '100%', width: '100%'}}
     />
   )
